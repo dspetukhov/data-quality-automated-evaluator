de64ad8024b318832320d95511cb1709fe827cca
dspetukhov <dspetukhov.work@gmail.com>
2025-09-21 12:20:25 +0400

    dev: handle exceptions + refactoring

--- main.py
+++ main.py
@@ -4,34 +4,17 @@ from utils import logging
 from analysis import make as make_analysis
 from report import make as make_report
 
-# Configuration
-# config = {
-    # "date_column": None,
-    # "target_column": "target",
-    # "numerical_columns": ["feature1", "feature2"],
-    # "categorical_columns": ["category1", "category2"],
-    # 'data_source': '../datasets/online-retail.xlsx'
-# }
 
-# Run analysis
 if __name__ == "__main__":
     if os.path.exists("config.json"):
         with open("config.json") as file:
-            config = json.load(file)
-        df, metadata = make_analysis(config)
-        make_report(df, metadata, config)
+            try:
+                config = json.load(file)
+            except json.JSONDecodeError as e:
+                logging.error(f"Error parsing configuration file: {e}")
+                conifg = None
+        if config and os.path.exists(config.get("source")):
+            df, metadata = make_analysis(config)
+            make_report(df, metadata, config)
     else:
-        logging.warning("Config file wasn't found.")
-
-
-# results = make_analysis(config)
-# make_report(results)
-# Access results
-# general_stats = results["results"]["general"]
-# categorical_analysis = results["results"]["categorical"]
-# numerical_analysis = results["results"]["numerical"]
-
-
-
-# Display plots
-# plt.show()
+        logging.warning("Configuration file wasn't found.")
