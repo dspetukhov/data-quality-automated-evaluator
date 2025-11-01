16d737d219735e49f6519c1bc71952b2bfd69537
dspetukhov <dspetukhov.work@gmail.com>
2025-11-01 13:29:03 +0400

    dev: refactoring & documenting

--- report.py
+++ report.py
@@ -1,10 +1,10 @@
 import os
-from tabulate import tabulate
 from pathlib import Path
-from typing import Dict, List, Union, Any, Tuple
+from typing import Dict, Union, Any, Tuple, List
 from polars import LazyFrame
 from utils import plot_data
-from analysis import exception_handler
+from utility import exception_handler
+from tabulate import tabulate
 
 
 @exception_handler()
@@ -24,7 +24,10 @@ def make_report(
     Returns:
         None
     """
-    output = config.get("output", "output")
+    output = config.get(
+        "output",
+        Path(config["source"]).name.split(".")[0]
+    )
     Path(output).mkdir(exist_ok=True)
 
     # markdown definitions
@@ -50,7 +53,7 @@ def make_report(
             "Number of values",
             "Target average" if "__target" in lf.columns else None)
     )
-    collect_md(col, stats, md_toc, md_content, precision)
+    collect_md_file(col, stats, md_toc, md_content, precision)
 
     mapping = config.get("mapping", {})
     for col in metadata:
@@ -64,7 +67,7 @@ def make_report(
             file_path=f"{output}/{col}",
             titles=[mapping.get(el, el) for el in metadata[col]["common"]]
         )
-        collect_md(col, stats, md_toc, md_content, precision)
+        collect_md_file(col, stats, md_toc, md_content, precision)
 
         # Numeric datatypes if present
         if metadata[col].get("numeric"):
@@ -78,7 +81,7 @@ def make_report(
                 file_path=f"{output}/{col}__numeric",
                 titles=[mapping.get(el, el) for el in metadata[col]["numeric"]]
             )
-            collect_md(
+            collect_md_file(
                 col, stats,
                 md_toc, md_content,
                 suffix="__numeric",
@@ -86,40 +89,11 @@ def make_report(
                 dtype=metadata[col]["dtype"])
 
         md_content.append("[Back to the TOC](#toc)\n")
-    write_md(md_toc, md_content, output, config["source"])
+    write_md_file(md_toc, md_content, output, config["source"])
 
 
-@exception_handler(exit_on_error=True)
-def write_md(
-        toc: List[Tuple[str, str]],
-        content: List[str],
-        output: str,
-        source: str
-) -> None:
-    """
-    Make and write a markdown file with table of contents and content.
-
-    Args:
-        toc (list): List of tuples (section name, anchor).
-        content (list): List of markdown content strings.
-        output (str): Output directory path.
-        source (str): Source file path.
-
-    Returns:
-        None
-    """
-    toc = [
-        f"- [{section}](#{anchor})"
-        for anchor, section in toc
-    ]
-    with open(os.path.join(output, "README.md"), "w") as f:
-        f.write(  # possible exception
-            "# Preliminary analysis for **`{}`**\n\n".format(source) +
-            "<a name='toc'></a>\n" + "\n".join(toc) + "\n\n" +
-            "\n".join(content))
-
-
-def collect_md(col, data, toc, content, precision=4, **kwargs) -> List:
+@exception_handler()
+def collect_md_file(col, data, toc, content, precision=4, **kwargs) -> List:
     """Process data to create Markdown file
     by updating lists for TOC and content in-place."""
     alias = kwargs.get("dtype", "Overview" if col == "__overview" else col)
@@ -144,17 +118,25 @@ def collect_md(col, data, toc, content, precision=4, **kwargs) -> List:
 @exception_handler()
 def make_md_table(data, precision) -> str:
     """
-    Create a markdown table from a dictionary with calculated statistics.
+    Create a markdown table from a dictionary of statistics.
+
+    This function converts a list of dictionaries
+    with statistics of aggregated column
+    into a markdown table using the tabulate library.
+    The table is returned as a string
+    to be included as a part of the markdown report.
 
     Args:
-        data (dict): Dictionary to be converted to a table.
+        data (List[dict]): The list of dictionaries with calculated statistics.
+        precision (int): Number of decimal places for numbers formatting.
 
     Returns:
-        str: Markdown table as an HTML code.
+        str: Markdown table.
     """
     sample = next((el for el in data if el))
     rows = [
-        [" " if key == "title" else f"**{key}**"] + [format_number(el[key], precision) for el in data]
+        [" " if key == "title" else f"**{key}**"] +
+        [format_number(el.get(key, ""), precision) for el in data]
         for key in sample
     ]
     return tabulate(
@@ -165,8 +147,52 @@ def make_md_table(data, precision) -> str:
     ) + "\n"
 
 
+@exception_handler(exit_on_error=True)
+def write_md_file(
+        toc: List[Tuple[str, str]],
+        content: List[str],
+        output: str,
+        source: str
+) -> None:
+    """
+    Create README.md file with table of contents and content.
+
+    Args:
+        toc (list): List of tuples for a markdown table of content (section name, anchor).
+        content (list): List of strings representing markdown content.
+        output (str): Output directory path.
+        source (str): Source file path.
+
+    Returns:
+        None
+    """
+    toc = [
+        f"- [{section}](#{anchor})"
+        for anchor, section in toc
+    ]
+    with open(os.path.join(output, "README.md"), "w") as f:
+        f.write(  # possible exception
+            "# Preliminary analysis for **`{}`**\n\n".format(source) +
+            "<a name='toc'></a>\n" + "\n".join(toc) + "\n\n" +
+            "\n".join(content))
+
+
+@exception_handler()
 def format_number(value, precision):
-    """Format float numbers with specified precision."""
+    """
+    Format float numbers with specified precision.
+
+    This function formats a float or a tuple of floats to a string with
+    the given number of decimal places,
+    otherwise it returns it as a string unchanged.
+
+    Args:
+        value (Union[float, tuple]): The number or a tuple of numbers to format.
+        precision (int): The number of decimal places to format to.
+
+    Returns:
+        str: The formatted number(s) as a string.
+    """
     if isinstance(value, float):
         if len(str(value).split(".")[1]) > precision:
             value = f"{value:,.{precision}f}"
