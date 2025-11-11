2ec6b141b8d2b915073ea76c93098ede230e1332
dspetukhov <dspetukhov.work@gmail.com>
2025-11-11 11:30:16 +0400

    dev: restructuring

---
+++ evaluate.py
@@ -0,0 +1,107 @@
+from typing import Sequence, Dict, Tuple, Union
+from utility import exception_handler
+
+
+@exception_handler()
+def evaluate_data(
+        data: Sequence[float],
+        config: Dict[str, Union[int, float]]
+) -> Tuple[Dict[str, float], Tuple[float]]:
+    """
+    Evaluates descriptive statistic and detects outliers in data.
+
+    This function calculates descriptive statistics and detects outliers
+    in data using IQR and Z-score criteria. Configuration dictionary specifies
+    IQR multiplier and Z-score threshold for outliers detection.
+
+    Args:
+        data (Sequence[float]): Data as Polars series.
+        config (Dict[str, Union[int, float]]): Configuration with parameters for outlier detection:
+            - 'multiplier' (float): IQR multiplier (default 1.5).
+            - 'threshold' (float): Z-score threshold (default 3.0).
+
+    Returns:
+        Tuple[Dict[str, float], Tuple[float]]:
+            - Dictionary with statistics:
+                - Mean and standard deviation,
+                - Range of values,
+                - Q1 and Q3,
+                - Outliers percentage according to IQR and Z-score criteria.
+            - Tuple with boundaries for outliers to be highlighted on plots.
+
+    """
+    # Calculate mean and standard deviation, first and third quartile
+    mean, std = data.mean(), data.std()
+    q1, q3 = data.quantile(0.25), data.quantile(0.75)
+
+    outliers_iqr, outliers_zscore, bounds = evaluate_data_outliers(
+        data, mean, std, q1, q3, config
+    )
+    return {
+        "μ±σ": (mean, std),
+        "Range [Min]": data.min(),
+        "Range [Max]": data.max(),
+        "Range": data.max() - data.min(),
+        "IQR [Q1]": q1,
+        "IQR [Q3]": q3,
+        "IQR": q3 - q1,
+        "Anomalies [IQR]": 100 * outliers_iqr / len(data),
+        "Anomalies [Z-score]": 100 * outliers_zscore / len(data),
+    }, bounds
+
+
+@exception_handler()
+def evaluate_data_outliers(
+        data: Sequence[float],
+        mean: float, std: float,
+        q1: float, q3: float,
+        config: Dict[str, Union[int, float]]
+) -> Tuple[int, Tuple[float]]:
+    """
+    Evaluates outliers in data.
+
+    This function calculates the number of outliers
+    according to IQR and Z-score criteria, determines boundaries
+    to highlight outliers on plots if criterion was specified in configuration.
+
+    Args:
+        data (Sequence[float]): Sequence of data as Polars series.
+        mean (float): Average of data.
+        std (float): Standard deviation.
+        q1 (float): First quartile.
+        q3 (float): Third quartile.
+        config (Dict[str, Union[int, float]]): Configuration with parameters for outlier detection:
+            - 'multiplier' (float): IQR multiplier (default 1.5).
+            - 'threshold' (float): Z-score threshold (default 3.0).
+
+    Returns:
+        Tuple[int, Tuple[float]]: Number of outliers based on IQR and Z-score,
+            boundaries to highlight outliers on plots.
+    """
+    # Count the number of outliers based on Z-score
+    if std == 0:
+        outliers_zscore = 0
+    else:
+        outliers_zscore = (
+            ((data - mean) / std).abs() > config.get("threshold", 3.0)
+        ).sum()
+
+    # Determine boundaries for outliers based on IQR
+    lower_bound = q1 - config.get("multiplier", 1.5) * (q3 - q1)
+    upper_bound = q3 + config.get("multiplier", 1.5) * (q3 - q1)
+    # Count the number of outliers
+    outliers_iqr = ((data < lower_bound) | (data > upper_bound)).sum()
+
+    # Get boundaries to highlight outliers on plots
+    # if criterion was specified in configuration
+    if config.get("criterion") == "Z-score":
+        bounds = (
+            mean - config.get("threshold", 3.0) * std,
+            mean + config.get("threshold", 3.0) * std
+        )
+    elif config.get("criterion") == "IQR":
+        bounds = (lower_bound, upper_bound)
+    else:
+        bounds = (None, None)
+
+    return outliers_iqr, outliers_zscore, bounds
