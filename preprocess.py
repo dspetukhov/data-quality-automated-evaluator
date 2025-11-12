32089a712ec2848821128beb0827c3fe96c8366d
dspetukhov <dspetukhov.work@gmail.com>
2025-11-12 18:09:58 +0400

    dev: restructuring

--- preprocess.py
+++ preprocess.py
@@ -1,6 +1,6 @@
 import polars as pl
 import polars.selectors as cs
-from typing import Dict, Any, Tuple, Union
+from typing import Dict, Any, List, Tuple, Union
 from utility import logging, exception_handler
 
 
@@ -34,17 +34,19 @@ def make_preprocessing(
     if isinstance(lf, pl.DataFrame):
         lf = lf.lazy()
 
-    # !rethink filtration and transformation
-    if "filtration" in config:
-        # Apply filter for rows
-        lf = apply_transformation(lf, config["filtration"], f=True)
-    if "transformation" in config:
+    lf = apply_filter(lf, config.get("filter"))
+    # if "filtration" in config:
+    #     # Apply filter for rows
+    #     lf = apply_transformation(lf, config["filtration"], f=True)
+    # if "transformation" in config:
         # Apply transformation for columns
-        lf = apply_transformation(lf, config["transformation"])
+    lf = apply_transformations(lf, config["transformations"])
 
     schema = lf.collect_schema()
+    # ! rethink date_column preparation 
     # Get date_column from configuration or try to find it in data
     # separate into single function
+    # get_date_column()
     date_column = config.get("date_column", find_date_column(schema))
     if date_column:
         # Check date_column type and convert it if necessary
@@ -62,6 +64,7 @@ def make_preprocessing(
     # Start with common aggregation expression for the number of values
     aggs = [pl.count().alias(" __Number of values")]
 
+    # add a separate function for target_column
     # If target column was specified in configuration,
     # compute its mean (class balance or target average)
     # ! separate into single function
@@ -72,33 +75,8 @@ def make_preprocessing(
     else:
         logging.warning("Target column wasn't found")
 
-    # collect_aggregations() implement!
-    metadata = {}
-    for col in schema.names():
-        if col == date_column:
-            continue
-        # Add common statistics for the column
-        aggs.extend([
-            # Number of unique values
-            pl.col(col).n_unique().alias(
-                f"__ {col} __Number of unique values"),
-            # Ratio of null values
-            pl.col(col).is_null().mean().alias(
-                f"__ {col} __Ratio of null values"),
-        ])
-
-        # Add extra statistics if column is of numeric data type
-        if col in cs.expand_selector(schema, cs.numeric()):
-            aggs.extend([
-                pl.col(col).min().alias(f"n__ {col} __Min"),
-                pl.col(col).max().alias(f"n__ {col} __Max"),
-                pl.col(col).mean().alias(f"n__ {col} __Mean"),
-                pl.col(col).median().alias(f"n__ {col} __Median"),
-                pl.col(col).std().alias(f"n__ {col} __Standard deviation"),
-            ])
-            metadata[col] = str(schema[col])
-        else:
-            metadata[col] = None
+    # Collect aggregation expressions for all columns except date_column
+    aggs, metadata = collect_aggregations(aggs, schema)
 
     # Aggregate data by date column
     lf_agg = lf.group_by("__date").agg(aggs).sort("__date")
@@ -108,27 +86,30 @@ def make_preprocessing(
 
 
 @exception_handler()
-def find_date_column(schema: pl.LazyFrame.schema) -> Union[str, None]:
+def apply_filter(
+        lf: pl.LazyFrame, ft: Union[str, Dict[str, str]], f: bool = False
+) -> pl.LazyFrame:
     """
-    Find the first date column in data schema.
+    Apply filters and transformations to a Polars LazyFrame.
 
-    This function iterates through data schema
-    and returns the first column of date or datetime type.
+    This function applies SQL-based filters and transformations as specified in
+    the configuration dictionary. If 'f' is True, a filter applied
+    to make a slice of data, otherwise transformation applied
+    to alter an existing column.
 
     Args:
-        schema (polars.LazyFrame.schema): Data schema provided by Polars.
+        lf (pl.LazyFrame): Input data as a LazyFrame.
+        ft (Union[str, Dict[str, str]]): Filter or transformation.
+        f (bool): If True, treat as a filter; otherwise, as a transformation.
 
     Returns:
-        Union[str, None]: Name of the first date or datetime column found,
-            or None if no such column exists.
+        pl.LazyFrame: Filtered or transformed LazyFrame.
     """
-    for col, dtype in schema.items():
-        if dtype in (pl.Date, pl.Datetime):
-            return col
+    return lf
 
 
 @exception_handler()
-def apply_transformation(
+def apply_transformations(
         lf: pl.LazyFrame, ft: Union[str, Dict[str, str]], f: bool = False
 ) -> pl.LazyFrame:
     """
@@ -158,3 +139,78 @@ def apply_transformation(
     else:
         logging.warning(f"Unrecognized transformation: {ft}")
     return lf
+
+# TO DO: modify
+@exception_handler()
+def get_date_column(schema: pl.LazyFrame.schema) -> Union[str, None]:
+    """
+    Find the first date column in data schema.
+
+    This function iterates through data schema
+    and returns the first column of date or datetime type.
+
+    Args:
+        schema (polars.LazyFrame.schema): Data schema provided by Polars.
+
+    Returns:
+        Union[str, None]: Name of the first date or datetime column found,
+            or None if no such column exists.
+    """
+    for col, dtype in schema.items():
+        if dtype in (pl.Date, pl.Datetime):
+            return col
+
+
+@exception_handler()
+def get_target_column(schema: pl.LazyFrame.schema) -> Union[str, None]:
+    """
+    Find the first date column in data schema.
+
+    This function iterates through data schema
+    and returns the first column of date or datetime type.
+
+    Args:
+        schema (polars.LazyFrame.schema): Data schema provided by Polars.
+
+    Returns:
+        Union[str, None]: Name of the first date or datetime column found,
+            or None if no such column exists.
+    """
+    for col, dtype in schema.items():
+        if dtype in (pl.Date, pl.Datetime):
+            return col
+        
+
+@exception_handler()
+def collect_aggregations(aggs: List[str], schema, date) -> Tuple[List[str], Dict[str]]:
+    """
+
+    """
+    metadata = {}
+    for col in schema.names():
+        if col == date_column:
+            continue
+        # Add common statistics for the column
+        aggs.extend([
+            # Number of unique values
+            pl.col(col).n_unique().alias(
+                f"__ {col} __Number of unique values"),
+            # Ratio of null values
+            pl.col(col).is_null().mean().alias(
+                f"__ {col} __Ratio of null values"),
+        ])
+
+        # Add extra statistics if column is of numeric data type
+        if col in cs.expand_selector(schema, cs.numeric()):
+            aggs.extend([
+                pl.col(col).min().alias(f"n__ {col} __Min"),
+                pl.col(col).max().alias(f"n__ {col} __Max"),
+                pl.col(col).mean().alias(f"n__ {col} __Mean"),
+                pl.col(col).median().alias(f"n__ {col} __Median"),
+                pl.col(col).std().alias(f"n__ {col} __Standard deviation"),
+            ])
+            metadata[col] = str(schema[col])
+        else:
+            metadata[col] = None
+
+    return aggs, metadata
