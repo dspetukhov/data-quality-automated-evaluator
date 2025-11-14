8c7b4b13e6a64516e4e775c69d7c676c9b00c2aa
dspetukhov <dspetukhov.work@gmail.com>
2025-11-14 16:14:09 +0400

    dev: changed source specification

--- utility/handle_data.py
+++ utility/handle_data.py
@@ -1,8 +1,9 @@
 import polars as pl
 from typing import Union, Any, Dict
 from .handle_exceptions import exception_handler
+from .setup_logging import logging
 
-# {file extension: read function} mapping
+# {file format: read function} mapping
 read_source_func = {
     "csv": pl.scan_csv,
     "parquet": pl.scan_parquet,
@@ -40,64 +41,97 @@ def read_source(source: Union[str, Dict[str, str]]) -> pl.LazyFrame:
     if isinstance(source, dict):
         if source.get("query") and source.get("uri"):
             # Read from PostgreSQL database
-            return pl.read_database_uri(
+            lf = pl.read_database_uri(
                 query=source["query"], uri=source["uri"]
             ).lazy()
-        elif "file_path" in source:
+        elif source.get("file_path"):
             # Get storage_options to read from cloud providers
             storage_options = source.get("storage_options")
             # Get schema_overrides to alter schema dtypes for csv / xlsx
-            schema_overrides = source.get("schema_overrides")
-            # Get read function if extension parameter was specified
-            read = read_source_func.get(source.get("extension"))
-            # If extension wasn't specified or read function wasn't found
-            if read is None:
-                return get_read_source_func(
-                    source["file_path"], storage_options)
-            else:
-                return read(
+            schema_overrides = handle_schema_overrides(
+                source.get("schema_overrides"))
+            # Get file format if specified
+            file_format = source.get("file_format")
+            lf = _read_source(
                     source["file_path"],
-                    storage_options=storage_options).lazy()
-        else:
-            raise SystemExit(f"Incorrect source specification: {source}")
-    elif isinstance(source, str):
-        return get_read_source_func(source)
-    else:
-        raise SystemExit("No source specification")
+                    file_format,
+                    storage_options, schema_overrides)
+
+        return lf
+
+    raise SystemExit(f"Incorrect source specification: {source}")
 
 
 @exception_handler(exit_on_error=True)
-def get_read_source_func(
+def _read_source(
         source: str,
-        storage_options: dict = None
+        file_format: str,
+        storage_options: Dict[str, str],
+        schema_overrides: Dict[str, str]
 ) -> pl.LazyFrame:
     """
-    Get function to read the source based on its ending.
+    Read source based on file format specified or based on source name ending.
 
-    This function iterates through `read_source_func` keys
-    matching them with source extension. If there is match,
-    it reads the source, otherwise SystemExit raised.
+    This function selects suitable read function
+    based on file format specified. If it wasn't specified,
+    it selects read function by matching source name ending
+    with supported file formats. If read function is found,
+    it reads source as Lazy data frame, otherwise SystemExit raised.
 
     Args:
         source (str): Data source name.
+        file_format (str): File format specified in configuration.
         storage_options (Dict[str, str]): Credentials to read from cloud providers.
+        schema_overrides (Dict[str, str]): Alters data types for specific columns
+            during schema inference.
 
     Returns:
         LazyFrame: Polars lazy data frame.
 
     Raises:
-        SystemExit: If source extension didn't match `read_source_func` keys.
+        SystemExit: If there is no read function for the file format provided.
     """
-    for extension, read in read_source_func.items():
-        if source.endswith(extension):
-            return read(source, storage_options=storage_options).lazy()
-    # Raise exception if source ending didn't match supported file extensions
-    raise SystemExit(f"Unsupported source: {source}")
+    if file_format in read_source_func:
+        read_func = read_source_func[file_format]
+    else:
+        # Try to match file extension with supported file formats
+        for ff, rf in read_source_func.items():
+            if source.endswith(ff):
+                logging.warning(f"Selected file format to read: {ff}")
+                file_format, read_func = ff, rf
+
+    if file_format in ("csv", "xlsx"):
+        lf = read_func(
+            source,
+            storage_options=storage_options,
+            schema_overrides=schema_overrides
+        ).lazy()
+    elif file_format in read_source_func:
+        lf = read_func(source, storage_options=storage_options).lazy()
+    else:
+        raise SystemExit(f"Unsupported source: {source}")
+
+    return lf
 
 
 @exception_handler()
 def handle_schema_overrides(data) -> Dict[str, Any]:
     """
-    
+    Replace string data type representation into Polars data type.
+
+    Args:
+        data (Dict[str, str]): Mapping of types representation
+            with Polars data types.
+
+    Returns:
+        Dict[str, Any]:
     """
+    dtypes = {
+        "String": pl.String,
+        "Date": pl.Date,
+        "Datetime": pl.Datetime
+    }
+    if isinstance(data, dict):
+        data = {key: dtypes[value] for key, value in data.items()}
+
     return data
