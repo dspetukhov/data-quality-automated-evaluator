1e9d8793505ae54c676a2121b8095c6069e6350e
dspetukhov <dspetukhov.work@gmail.com>
2025-11-14 11:47:46 +0400

    fix: restructuring + documenting

--- utility/handle_data.py
+++ utility/handle_data.py
@@ -2,15 +2,26 @@ import polars as pl
 from typing import Union, Dict
 from .handle_exceptions import exception_handler
 
+# {file extension: read function} mapping
+read_source_func = {
+    "csv": pl.scan_csv,
+    "parquet": pl.scan_parquet,
+    "iceberg": pl.scan_iceberg,
+    "xlsx": pl.read_excel
+}
+
 
 @exception_handler(exit_on_error=True)
 def read_source(source: Union[str, Dict[str, str]]) -> pl.LazyFrame:
     """
-    Read specified source of data into Polars LazyFrame.
+    Read specified source of data as Polars LazyFrame.
 
     Supports CSV, Parquet, Iceberg, XLSX, and database URIs.
     Returns LazyFrame for preprocessing. If loading fails,
     it terminates the main program.
+    There is no need in specifying `storage_options` explicitly
+    as Polars can read corresponding environmental variables,
+    e.g. `AWS_ACCESS_KEY_ID` / `AWS_SECRET_ACCESS_KEY` / `AWS_REGION`.
 
     Args:
         source (Union[str, Dict[str, str]]): Data source specification.
@@ -21,32 +32,11 @@ def read_source(source: Union[str, Dict[str, str]]) -> pl.LazyFrame:
             (`storage_options` and `extension` are optional).
 
     Returns:
-        LazyFrame: Polars lazy DataFrame.
+        LazyFrame: Polars lazy data frame.
 
     Raises:
         SystemExit: If data cannot be loaded.
     """
-    # {file extension: read function} mapping
-    read_source_func = {
-        "csv": pl.scan_csv,
-        "parquet": pl.scan_parquet,
-        "iceberg": pl.scan_iceberg,
-        "xlsx": pl.read_excel
-    }
-
-    def get_read_source_func(
-            source: str,
-            storage_options: dict = None
-    ) -> pl.LazyFrame:
-        """
-        Get function to read the source based on its ending.
-        """
-        for extension, read in read_source_func.items():
-            if source.endswith(extension):
-                return read(source, storage_options=storage_options).lazy()
-        # Raise exception if source ending didn't match supported file extensions
-        raise ValueError(f"Unsupported source: {source}")
-
     if isinstance(source, dict):
         if source.get("query") and source.get("uri"):
             # Read from PostgreSQL database
@@ -70,3 +60,32 @@ def read_source(source: Union[str, Dict[str, str]]) -> pl.LazyFrame:
             raise ValueError(f"Incorrect source specification: {source}")
     elif isinstance(source, str):
         return get_read_source_func(source)
+
+
+@exception_handler(exit_on_error=True)
+def get_read_source_func(
+        source: str,
+        storage_options: dict = None
+) -> pl.LazyFrame:
+    """
+    Get function to read the source based on its ending.
+
+    This function iterates through `read_source_func` keys
+    matching them with source extension. If there is match,
+    it reads the source, otherwise ValueError raised.
+
+    Args:
+        source (str): Data source name.
+        storage_options (Dict[str, str]): Credentials to read from cloud providers.
+
+    Returns:
+        LazyFrame: Polars lazy data frame.
+
+    Raises:
+        SystemExit: If source extension didn't match `read_source_func` keys.
+    """
+    for extension, read in read_source_func.items():
+        if source.endswith(extension):
+            return read(source, storage_options=storage_options).lazy()
+    # Raise exception if source ending didn't match supported file extensions
+    raise ValueError(f"Unsupported source: {source}")
