cd6279394c091fdb2e5ae5ff75f901228e7f7d25
dspetukhov <dspetukhov.work@gmail.com>
2026-01-27 17:26:10 +0400

    dev: revision

--- utility/__init__.py
+++ utility/__init__.py
@@ -1,3 +1,6 @@
 from .handle_exceptions import exception_handler
 from .setup_logging import logging
 from .handle_data import read_source
+
+TIME_INTERVAL_COL = "__time_interval"
+OVERVIEW_COL = "__overview"
